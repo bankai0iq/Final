7b6a83c0732e017acb093dd7cba36fc6f26e67f0
vlad <vojtk.stanislav@gmail.com>
2023-03-14 20:40:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] {"911", "98", "hello", "world", ":-)"};
+string[] array3 = new string[5] { "911", "98", "hello", "world", ":-)" };
+string[] array2 = new string[array1.Length];
+int count = 0;
+
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array2);
